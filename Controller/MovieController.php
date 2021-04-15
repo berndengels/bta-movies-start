5a25db7adef1be26d9a4d41af7808760894f16fc
Victoria <victoria.iv6765@gmail.com>
2021-04-15 11:53:58 +0200

    stuff

--- Controller/MovieController.php
+++ Controller/MovieController.php
@@ -11,6 +11,19 @@ class MovieController extends Controller {
 
     public function index() {
         $list = $this->model->all();
-        Helper::dump($list);
+        // Helper::dump($list);
+        if($this->auth) {
+            require_once 'Views/movie/admin/index.php';
+        } 
+        else {
+            require_once 'Views/movie/index.php';
+        }
+    }
+
+    public function show($id) {        
+        $authormodel = new Author();
+        $item = $this->model->find($id);
+        $item['author'] = $authormodel->find($item['author_id']);        
+        require_once 'Views/movie/show.php';
     }
 }
--- Models/Movie.php
+++ Models/Movie.php
@@ -5,6 +5,21 @@ class Movie extends Model {
 
     protected $table = 'movies';
 
+    public function all() {
+        $sql = "SELECT * FROM $this->table";
+        return $this->getAll($sql);
+    }
+
+    public function find(int $id) {
+        // numeric parameters
+        $sql = "SELECT * FROM $this->table WHERE id=?";
+        return $this->getOne($sql, [$id]);
+
+        // assoc parameters
+        $sql = "SELECT * FROM $this->table WHERE id=:id";
+        return $this->getOne($sql, ['id' => $id]);
+    }
+
 /*
     public function insert(array $params) {
         $sql = "INSERT INTO authors (firstname, lastname) VALUES (:firstname, :lastname)";
